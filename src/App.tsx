c464fdeb79977926d8a1202537f8abff48c1be84
lhk3337 <lhk3337@naver.com>
2022-04-22 17:10:20 +0900

    tsx init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import DailyEditor from "./DailyEditor";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <DailyEditor />
+    </div>
+  );
+}
+
+export default App;
