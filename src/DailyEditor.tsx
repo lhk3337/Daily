f7f6e0c12e3b9c28465ca1056a8212e37db3e9b3
lhk3337 <lhk3337@naver.com>
2022-04-22 17:12:14 +0900

    useState를 여러개로 설정

--- src/DailyEditor.tsx
+++ src/DailyEditor.tsx
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
 const DailyEditor = () => {
-  //   const [author, setAuthor] = useState("");
-  //   const [content, setContent] = useState("");
-
-  const [state, setState] = useState({
-    author: "",
-    content: "",
-  });
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
   const onReset = () => {
-    setState({ author: "", content: "" });
+    setAuthor("");
+    setContent("");
   };
 
   return (
@@ -17,16 +13,15 @@ const DailyEditor = () => {
       <div className="container">
         <input
           type="text"
-          value={state.author}
+          value={author}
           onChange={(e) => {
-            setState({ ...state, author: e.target.value });
+            setAuthor(e.target.value);
           }}
         />
-        <textarea value={state.content} onChange={(e) => setState({ ...state, content: e.target.value })} />
+        <textarea value={author} onChange={(e) => setContent(e.target.value)} />
         <button onClick={onReset}>초기화</button>
       </div>
-      <div>{state.author}</div>
-      <div>{state.content}</div>
+      <div>{content}</div>
     </div>
   );
 };
