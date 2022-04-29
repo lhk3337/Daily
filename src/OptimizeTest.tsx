f51d9878041d6e4ef2b14e6c04190b2db1637fd6
lhk3337 <lhk3337@naver.com>
2022-04-29 10:48:26 +0900

    React.memo 2

--- src/OptimizeTest.tsx
+++ src/OptimizeTest.tsx
@@ -1,42 +1,62 @@
 import React, { useState, useEffect } from "react";
 
-interface Icount {
-  text?: string;
-  count?: number;
-}
-
-const Textview = React.memo(({ text }: Icount) => {
+const CountA = React.memo(({ count }: any) => {
   useEffect(() => {
-    console.log(`Update :: Text : ${text}`);
-  });
-  return <div>{text}</div>;
-});
-
-const CountView = React.memo(({ count }: Icount) => {
-  useEffect(() => {
-    console.log(`Update :: Count : ${count}`);
+    console.log(`CountA : ${count}`);
   });
   return <div>{count}</div>;
 });
 
+const CountB = ({ obj }: any) => {
+  useEffect(() => {
+    console.log(`CountB : ${obj.count}`);
+  });
+  return <div>{obj.count}</div>;
+};
+
+const areEqual = (prevProps: any, nextProps: any) => {
+  //   if (prevProps.obj.count === nextProps.obj.count) {
+  //     return true;
+  //   }
+  //   return false;  return prevProps.obj.count === nextProps.obj.count;와 같음
+  return prevProps.obj.count === nextProps.obj.count;
+
+  //   return true; //  prevProps === nextProps -> 리렌더링을 일으키지 않게 됨
+  //   return false; // prevProps !== nextProps -> 리렌더링을 일으킴
+};
+
+const MemoizedCounterB = React.memo(CountB, areEqual);
+
 const OptimizeTest = () => {
   const [count, setCount] = useState<number>(1);
-  const [text, setText] = useState<string>("");
+  const [obj, setObj] = useState({
+    count: 1,
+  });
 
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>count</h2>
-        <CountView count={count} />
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Count A</h2>
+        <CountA count={count} />
+        <button onClick={() => setCount(count)}>A button</button>
       </div>
       <div>
-        <h2>text</h2>
-        <Textview text={text} />
-        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        <h2>Counter B</h2>
+        <MemoizedCounterB obj={obj} />
+        <button
+          onClick={() =>
+            setObj({
+              count: obj.count,
+            })
+          }
+        >
+          B button
+        </button>
       </div>
     </div>
   );
 };
 
 export default OptimizeTest;
+
+// React.memo 얕은 복사일떄는 렌더링이 일아나고 깊은 복사일떄는 리렌더링이 일어나지 않는다.
