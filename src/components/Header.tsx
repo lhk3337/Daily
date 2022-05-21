32ac324a8c11785aff3f3bd2e2c64c3de15f443a
lhk3337 <lhk3337@naver.com>
2022-05-21 23:26:06 +0900

    [FEAT] header component -  add button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="font-bold">
-      <div>
-        <button></button>
+    <header className="font-bold flex py-5 border-b border-[#e2e2e2] justify-between items-center">
+      <div className="w-3/12">
+        <button className="px-5 py-2.5 bg-[#ececec] rounded">&lt;</button>
       </div>
       <h1>2022년 5월</h1>
-      <div></div>
-    </div>
+      <div className="w-3/12">
+        <button className="px-5 py-2.5 bg-[#ececec] rounded">&gt;</button>
+      </div>
+    </header>
   );
 };
 
