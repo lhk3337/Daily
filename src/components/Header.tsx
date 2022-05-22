344cfcbc25413933b7ed6eddb988af2c28fd392d
lhk3337 <lhk3337@naver.com>
2022-05-22 21:58:39 +0900

    [FIX] header component -  button css modify

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="font-bold flex py-5 border-b border-[#e2e2e2] justify-between items-center">
-      <div className="w-3/12">
-        <button className="px-5 py-2.5 bg-[#ececec] rounded">&lt;</button>
+    <header className="flex py-5 border-b border-[#e2e2e2] items-center  w-full">
+      <div className="w-3/12 justify-start flex">
+        <button className="px-5 py-2.5 bg-[#ececec] rounded whitespace-nowrap">&lt;</button>
       </div>
-      <h1>2022년 5월</h1>
-      <div className="w-3/12">
-        <button className="px-5 py-2.5 bg-[#ececec] rounded">&gt;</button>
+      <h1 className="w-1/2 text-2xl justify-center font-bold flex">2022년 5월</h1>
+      <div className="w-3/12 justify-end flex">
+        <button className="px-5 py-2.5 bg-[#ececec] rounded whitespace-nowrap">&gt;</button>
       </div>
     </header>
   );
