1fe122f29b0d673d33e229460ba8d1c5909b3e76
lhk3337 <lhk3337@naver.com>
2022-05-24 22:20:33 +0900

    [FEAT] - Add Home Component

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { useLocation, useParams, useNavigate } from "react-router-dom";
 const Home = () => {
-  return <div>Home</div>;
+  const navi = useNavigate();
+
+  return (
+    <div>
+      <button
+        className="w-[232px]  bg-[#64C964] rounded px-5 py-2.5 text-white box-border text-lg"
+        onClick={() => navi("/new")}
+      >
+        새 일기쓰기
+      </button>
+    </div>
+  );
 };
 
 export default Home;
