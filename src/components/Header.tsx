2fc24299cac8b53e22b6ea37350571d49a2233ab
lhk3337 <lhk3337@naver.com>
2022-05-25 18:52:27 +0900

    [FEAT] Header Component 날짜 월별 증가 감소 버튼 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,56 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 const Header = () => {
+  const location = useLocation();
+  const Navi = useNavigate();
+
+  const [date, setDate] = useState<Date>(new Date());
+
+  const onLefthandleClick = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() - 1, date.getDate()));
+  };
+  const onRighthandleClick = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() + 1, date.getDate()));
+  };
+
   return (
     <header className="flex py-5 border-b border-[#e2e2e2] items-center  w-full">
-      <div className="w-3/12 justify-start flex">
-        <button className="px-5 py-2.5 bg-[#ececec] rounded whitespace-nowrap">&lt;</button>
-      </div>
-      <h1 className="w-1/2 text-2xl justify-center font-bold flex">2022년 5월</h1>
-      <div className="w-3/12 justify-end flex">
-        <button className="px-5 py-2.5 bg-[#ececec] rounded whitespace-nowrap">&gt;</button>
-      </div>
+      {location.pathname === "/" ? (
+        <>
+          <div className="w-3/12 justify-start flex">
+            <button
+              onClick={() => {
+                onLefthandleClick();
+              }}
+              className="px-5 py-2.5 bg-[#ececec] rounded whitespace-nowrap"
+            >
+              &lt;
+            </button>
+          </div>
+          <h1 className="w-1/2 text-2xl justify-center font-bold flex">{`${date.getFullYear()}년 ${
+            date.getMonth() + 1
+          }월`}</h1>
+          <div className="w-3/12 justify-end flex">
+            <button onClick={() => onRighthandleClick()} className="px-5 py-2.5 bg-[#ececec] rounded whitespace-nowrap">
+              &gt;
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="w-3/12 justify-start flex">
+            <button
+              onClick={() => {
+                Navi("/");
+              }}
+              className="px-5 py-2.5 bg-[#ececec] rounded whitespace-nowrap"
+            >
+              &lt; 뒤로가기
+            </button>
+          </div>
+          <h1 className="w-1/2 text-2xl justify-center font-bold flex">새 일기쓰기</h1>
+        </>
+      )}
     </header>
   );
 };
