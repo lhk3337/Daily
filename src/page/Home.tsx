2b2dce7fd00385b1498939e5a2b3dc5f59f2d12f
lhk3337 <lhk3337@naver.com>
2022-05-27 22:48:01 +0900

    [FEAT] - dummy data redux 적용

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { useLocation, useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Home = () => {
   const navi = useNavigate();
 
+  const state = useSelector(({ diary }: any) => diary.data);
+
+  console.log(state);
+
   return (
-    <div>
+    <div className="mt-[20px] mb-[30px]">
       <button
         className="w-[232px]  bg-[#64C964] rounded px-5 py-2.5 text-white box-border text-lg"
         onClick={() => navi("/new")}
       >
         새 일기쓰기
       </button>
+      <ul>
+        {state.map((item: any, idx: number) => (
+          <li key={idx}>{item.content}</li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/store/modules/diary.ts
+++ src/store/modules/diary.ts
@@ -2,6 +2,7 @@ const INIT = "INIT";
 const CREATE = "CREATE";
 const DELETE = "DELETE";
 const EDIT = "EDIT";
+
 type actionType = {
   type: string;
   data: { id: string };
@@ -9,12 +10,23 @@ type actionType = {
   date: Date;
 };
 
-export default function Diary(state: [], action: actionType) {
+const data = [
+  { id: 0, content: "오늘의 일기 0번", emotion: 3, date: 1653656723014 },
+  { id: 1, content: "오늘의 일기 1번", emotion: 5, date: 1653656764897 },
+  { id: 2, content: "오늘의 일기 2번", emotion: 1, date: 1653656786274 },
+  { id: 3, content: "오늘의 일기 3번", emotion: 4, date: 1653656799088 },
+  { id: 4, content: "오늘의 일기 4번", emotion: 2, date: 1653656810527 },
+];
+
+const initialState = {
+  data,
+};
+
+export default function Diary(state: any = initialState, action: actionType) {
   let newState = [];
   switch (action.type) {
     case INIT:
-      return action.data;
-
+      return state;
     case CREATE: {
       newState = [action.data, ...state];
       break;
