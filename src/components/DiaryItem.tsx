8c1f987fa5421f0a0f07a611819e67248cbbe98c
lhk3337 <lhk3337@naver.com>
2022-06-06 20:32:58 +0900

    [FEAT] home - diary list css styling

--- src/components/DiaryItem.tsx
+++ src/components/DiaryItem.tsx
@@ -1,24 +1,39 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 interface Iprops {
   emotion: number;
   content: string;
   date: number;
+  id: number;
 }
 
-const DiaryItem = ({ emotion, content, date }: Iprops) => {
+const DiaryItem = ({ emotion, content, date, id }: Iprops) => {
+  const navi = useNavigate();
   const emotionContainerColor = ["bg-[#64C965]", "bg-[#9DD772]", "bg-[#FECE15]", "bg-[#FD8446]", "bg-[#FD565F]"];
   return (
-    <div>
-      <div className={`${emotionContainerColor[emotion - 1]}`}>
-        <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} alt={`emotion${emotion}`} />
+    <div className="py-[15px] cursor-pointer flex justify-between border-b border-b-[#e4e4e4]">
+      <div
+        className={`${emotionContainerColor[emotion - 1]} rounded flex justify-center min-w-[120px] h-20`}
+        onClick={() => navi(`/diary/${id}`)}
+      >
+        <img
+          src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`}
+          alt={`emotion${emotion}`}
+          className="w-1/2"
+        />
+      </div>
+      <div className="flex-grow ml-5 cursor-pointer" onClick={() => navi(`/diary/${id}`)}>
+        <h1 className="text-[25px] font-bold">{new Date(date).toLocaleDateString("ko-KR")}</h1>
+        <h1 className="text-[18px]">{content}</h1>
       </div>
       <div>
-        <h2>{new Date(date).toLocaleDateString("ko-KR", { year: "numeric", month: "long", day: "numeric" })}</h2>
-        <h1>{content}</h1>
-      </div>
-      <div>
-        <button>수정하기</button>
+        <button
+          className="px-5 py-2 bg-[#ececec] text-[18px] rounded whitespace-nowrap"
+          onClick={() => navi(`/edit/${id}`)}
+        >
+          수정하기
+        </button>
       </div>
     </div>
   );
