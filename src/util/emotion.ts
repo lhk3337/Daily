e67b99e6754f1fdbee626fc9ac21df37ea198930
lhk3337 <lhk3337@naver.com>
2022-06-08 21:01:17 +0900

    [FIX] emotion 버튼 데이터 추가

---
+++ src/util/emotion.ts
@@ -0,0 +1,7 @@
+export const emotionList = [
+  { id: 1, imgUrl: process.env.PUBLIC_URL + `assets/emotion1.png`, title: "완전 좋음", bgColor: "bg-[#64C965]" },
+  { id: 2, imgUrl: process.env.PUBLIC_URL + `assets/emotion2.png`, title: "좋음", bgColor: "bg-[#9DD772]" },
+  { id: 3, imgUrl: process.env.PUBLIC_URL + `assets/emotion3.png`, title: "그저그럭", bgColor: "bg-[#FECE15]" },
+  { id: 4, imgUrl: process.env.PUBLIC_URL + `assets/emotion4.png`, title: "나쁨", bgColor: "bg-[#FD8446]" },
+  { id: 5, imgUrl: process.env.PUBLIC_URL + `assets/emotion5.png`, title: "끔찍함", bgColor: "bg-[#FD565F]" },
+];
