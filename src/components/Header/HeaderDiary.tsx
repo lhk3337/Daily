5573fb357d4ad57d5a467bbe790ceb7910a99e9b
lhk3337 <lhk3337@naver.com>
2022-06-10 18:54:43 +0900

    [FEAT] 새로 만들기 Header 시간 설정 하기

--- src/components/Header/HeaderDiary.tsx
+++ src/components/Header/HeaderDiary.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { PathMatch } from "react-router-dom";
-
+import { RootState } from "store/modules";
+import { elementDataType } from "store/modules/diary";
 interface Iprops {
   Navi: any;
   pathInfo: PathMatch<string>;
@@ -10,6 +12,11 @@ const HeaderDiary = ({ Navi, pathInfo }: Iprops) => {
   const {
     params: { id },
   } = pathInfo;
+  const diary = useSelector(({ diary }: RootState) => diary);
+
+  const userData = diary?.filter((it: elementDataType) => it.id === Number(id));
+  const { date } = userData[0];
+  const offset = new Date().getTimezoneOffset() * 60000;
 
   return (
     <>
@@ -23,7 +30,10 @@ const HeaderDiary = ({ Navi, pathInfo }: Iprops) => {
           &lt; 뒤로가기
         </button>
       </div>
-      <h1 className="w-1/2 text-2xl justify-center font-bold flex">일기 기록</h1>
+      <h1 className="w-1/2 text-2xl justify-center font-bold flex">
+        {new Date(date - offset).toISOString().slice(0, 10)} 기록
+        {/* offset을 빼지않으면 하루전 출력, toISOString함수는 UTC시간을 기준으로 반환하기 때문인데, 한국 시간과 9시간 차이가 남 */}
+      </h1>
       <div className="w-3/12 justify-end flex">
         <button
           onClick={() => {
