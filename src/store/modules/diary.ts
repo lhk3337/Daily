2b621b08fa3ccdc6abbdcec09576b05f0c8969fb
lhk3337 <lhk3337@naver.com>
2022-06-10 18:35:01 +0900

    [FIX] - reducer create 액션 함수 수정

--- src/store/modules/diary.ts
+++ src/store/modules/diary.ts
@@ -4,12 +4,12 @@ const REMOVE = "REMOVE";
 const EDIT = "EDIT";
 
 const dummyData = [
-  { id: 0, content: "오늘의 일기 0번", emotion: 3, date: 1650646723014 },
-  { id: 1, content: "오늘의 일기 1번", emotion: 5, date: 1651656764897 },
-  { id: 2, content: "오늘의 일기 2번", emotion: 1, date: 1652656786274 },
-  { id: 3, content: "오늘의 일기 3번", emotion: 4, date: 1652756799088 },
-  { id: 4, content: "오늘의 일기 4번", emotion: 2, date: 1653956810527 },
-  { id: 5, content: "오늘의 일기 5번", emotion: 3, date: 1654242133171 },
+  { id: 10, content: "오늘의 일기 0번", emotion: 3, date: 1650646723014 },
+  { id: 11, content: "오늘의 일기 1번", emotion: 5, date: 1651656764897 },
+  { id: 12, content: "오늘의 일기 2번", emotion: 1, date: 1652656786274 },
+  { id: 13, content: "오늘의 일기 3번", emotion: 4, date: 1652756799088 },
+  { id: 14, content: "오늘의 일기 4번", emotion: 2, date: 1653956810527 },
+  { id: 15, content: "오늘의 일기 5번", emotion: 3, date: 1654242133171 },
 ];
 
 interface IdummyDataData {
@@ -38,15 +38,12 @@ export const edit = (targetId: number, date: Date, content: string, emotion: num
 
 export const remove = (targetId: string) => ({ type: REMOVE, targetId: targetId });
 
-export const create = (
-  date: Date,
-  content: string,
-  emotion: number,
-  dataId: React.MutableRefObject<number>
-): actionType => ({
-  type: CREATE,
-  data: { id: dataId.current, date: new Date(date).getTime(), content, emotion },
-});
+export const create = (date: string, content: string, emotion: number, dataId: any): actionType => {
+  return {
+    type: CREATE,
+    data: { id: dataId, date: new Date(date).getTime(), content: content, emotion: emotion },
+  };
+};
 
 export default function Diary(state: IdummyDataDatas = dummyData, action: actionType) {
   let newState = [];
