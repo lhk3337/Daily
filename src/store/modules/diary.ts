c69b9d7e9af423773f4d3faa93064a5aff017d4e
lhk3337 <lhk3337@naver.com>
2022-06-16 19:55:44 +0900

    [FIX] - redux dummy data delete

--- src/store/modules/diary.ts
+++ src/store/modules/diary.ts
@@ -3,15 +3,6 @@ const CREATE = "CREATE";
 const REMOVE = "REMOVE";
 const EDIT = "EDIT";
 
-const dummyData = [
-  { id: 10, content: "오늘의 일기 0번", emotion: 3, date: 1650646723014 },
-  { id: 11, content: "오늘의 일기 1번", emotion: 5, date: 1651656764897 },
-  { id: 12, content: "오늘의 일기 2번", emotion: 1, date: 1652656786274 },
-  { id: 13, content: "오늘의 일기 3번", emotion: 4, date: 1652756799088 },
-  { id: 14, content: "오늘의 일기 4번", emotion: 2, date: 1653956810527 },
-  { id: 15, content: "오늘의 일기 5번", emotion: 3, date: 1654242133171 },
-];
-
 export interface elementDataType {
   id: number;
   content?: string;
@@ -48,7 +39,7 @@ export const create = (date: string, content: string, emotion: number) => {
 
 // ================================================================================================================
 
-export default function Diary(state: elementDataType[] = dummyData, action: actionType): any {
+export default function Diary(state: elementDataType[] = [], action: actionType): any {
   let newState = [];
   switch (action.type) {
     case CREATE: {
